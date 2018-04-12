192ddccd020c6013f6cd581d7de179a49e5999a0
kantapp inc <info@kantapp.com>
2018-04-12 13:34:25 +0530

    created user registration with image resize

--- DB/dbcon.php
+++ DB/dbcon.php
@@ -1,5 +1,6 @@
 <?php
-    error_reporting(0);
+    // error_reporting(0);
+    date_default_timezone_set("Asia/kolkata");
     $servername="localhost";
     $username="root";
     $password="";
@@ -11,7 +12,102 @@
         die("Failed To connect MySQL :".$dbcon->connect_error);
     }
     
-    function info()
+    //Regeter New User
+    function createUser($name,$gender,$dob,$imageb64,$email,$password)
     {
-        phpinfo();
+        global $dbcon;
+        if(!isUserExist($email))
+        {
+            $pass=md5($password);
+            $token=getApiKey();
+            if($imageb64=="" && $gender=="male")
+            {
+                $imageUrl="/img/male.png";
+            }
+            else if($imageb64=="" && $gender=="female")
+            {
+                $imageUrl="/img/female.png";
+            }
+            else 
+            {
+                if(file_put_contents("../img/profile/$token.png",base64_decode($imageb64)))
+                {
+
+                    ;
+                    
+
+                    if(Thumbnail("../img/profile/$token.png","../img/profile/profile_$token.png"))
+                    {
+                        $imageUrl="/img/profile/profile_$token.png";
+                    }  
+                    else 
+                    {
+                        $imageUrl="/img/male.png";
+                        unlink("../img/profile/$token.png");
+                    } 
+                    
+                }
+                
+            }
+            $stm=$dbcon->prepare("INSERT INTO `billionsmile_user` (`id`, `full_name`, `gender`, `dob`, `image_url`, `email`, `password`, `token`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?)");
+            $stm->bind_param("sssssss",$name,$gender,$dob,$imageUrl,$email,$pass,$token);
+            $result=$stm->execute();
+            $stm->close();
+            if($result)
+            {
+                return 0;
+            }
+            else {
+                return 1;
+            }
+        }
+        else {
+            return 2;
+        } 
+
+        
     }
+
+    function isUserExist($email)
+    {
+        global $dbcon;
+        $stm=$dbcon->prepare("SELECT * FROM `billionsmile_user` WHERE `email`= ?;");
+        $stm->bind_param("s",$email);
+        $stm->execute();
+        $stm->store_result();
+        $num_rows=$stm->num_rows;
+        $stm->close();
+        return $num_rows;
+    }
+    
+    function getApiKey()
+    {
+        return md5(uniqid(rand(),true));
+    }
+
+    function isBase64($s)
+    {
+        return (bool) preg_match('/^[a-zA-Z0-9\/\r\n+]*={0,2}$/', $s);
+    }
+
+    function Thumbnail($url, $filename, $width = 150, $height = true) 
+    {
+        // download and create gd image
+        $image = ImageCreateFromString(file_get_contents($url));
+       
+        if($image)
+        {
+            $height = $height === true ? (ImageSY($image) * $width / ImageSX($image)) : $height;
+            $output = ImageCreateTrueColor($width, $height);
+            ImageCopyResampled($output, $image, 0, 0, 0, 0, $width, $height, ImageSX($image), ImageSY($image));
+            ImageJPEG($output, $filename, 90);
+            return true;
+        }
+        else 
+        {
+            return false;
+        }        
+       
+    }
+
+    
--- v1/index.php
+++ v1/index.php
@@ -23,12 +23,67 @@ require __DIR__ . '/../src/dependencies.php';
 // Register middleware
 require __DIR__ . '/../src/middleware.php';
 
-// Register routes
-require __DIR__ . '/../src/routes.php';
 
-$app->any("/",function(Request $request,Response $response)
+
+$app->post("/usr/create",function(Request $request,Response $response)
 {
-    return $response->getBody()->write(info());
+    // return $response->withJson(createUser("Name","male","1992","","kanta1311@gmail.com","12345"), 200);
+    if(isTheseParametersAvailable(array('name','gender','dob','email','password')))
+    {
+        $key=$request->getParsedBody();
+        $name=$key["name"];
+        $gender=$key["gender"];
+        $dob=$key["dob"];
+        $email=$key["email"];
+        $password=$key["password"];
+        $imageb64=$key["imageb64"];
+        $result=createUser($name,$gender,$dob,$imageb64,$email,$password);
+
+        if($result==0)
+        {
+            $data=["error"=>false,"message"=>"You are successfully registered"];
+            return $response->withJson($data,201);
+        }
+        else if($result==1)
+        {
+            $data=["error"=>true,"message"=>"Oops! An error occurred while registereing"];
+            return $response->withJson($data,200);
+        }
+        else if($result==2)
+        {
+            $data=["error"=>true,"message"=>"Sorry, this User  already existed"];
+            return $response->withJson($data,200);
+        }
+
+        // return $response->withJson($result);
+
+        
+    }
+
 });
 // Run app
 $app->run();
+
+
+function isTheseParametersAvailable($required_fields)
+{
+    $error = false;
+    $error_fields = "";
+    $request_params = $_REQUEST;
+
+    foreach ($required_fields as $field) {
+        if (!isset($request_params[$field]) || strlen(trim($request_params[$field])) <= 0) {
+            $error = true;
+            $error_fields .= $field . ', ';
+        }
+    }
+
+    if ($error) {
+        $response = array();
+        $response["error"] = true;
+        $response["message"] = 'Required field(s) ' . substr($error_fields, 0, -2) . ' is missing or empty';
+        echo json_encode($response);
+        return false;
+    }
+    return true;
+}
